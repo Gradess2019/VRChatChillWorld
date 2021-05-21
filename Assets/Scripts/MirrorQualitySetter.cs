aa3953ee57586287be581e02302f33f41533099a
Gradess <trofimovjava@gmail.com>
2021-05-21 21:16:31 +0300

    Add mirror quality setter

---
+++ Assets/Scripts/MirrorQualitySetter.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MirrorQualitySetter : UdonSharpBehaviour
+{
+    [SerializeField] private VRC_MirrorReflection mirror;
+    [SerializeField] private LayerMask reflectLayers;
+    public override void Interact()
+    {
+        if (!mirror)
+        {
+            return;
+        }
+
+        mirror.m_ReflectLayers = reflectLayers;
+    }
+}
