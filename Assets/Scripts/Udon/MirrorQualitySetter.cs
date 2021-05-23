db84b61287bcbcc77d66ade19a857c71bb5ca4fc
Gradess <trofimovjava@gmail.com>
2021-05-23 20:19:25 +0300

    Add MirrorToggler

---
+++ Assets/Scripts/Udon/MirrorQualitySetter.cs
@@ -0,0 +1,23 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class MirrorQualitySetter : UdonSharpBehaviour
+{
+    [SerializeField] private LayerMask reflectLayers;
+
+    public LayerMask GetReflectLayers()
+    {
+        return reflectLayers;
+    }
+    
+    public void SetQuality(VRC_MirrorReflection targetMirror)
+    {
+        if (!targetMirror)
+        {
+            return;
+        }
+
+        targetMirror.m_ReflectLayers = reflectLayers;
+    }
+}
